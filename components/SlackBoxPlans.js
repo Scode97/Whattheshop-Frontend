5748f935d7b64a5723e39f9e7b4bb2f2e6dcbb4c
scode97 <sehameldeen1997@gmail.com>
2018-09-09 16:00:52 +0300

    update

--- components/SlackBoxPlans.js
+++ components/SlackBoxPlans.js
@@ -1,51 +1,47 @@
 import React, { Component } from "react";
-import { Text, View} from 'react-native';
-import {Card, CardItem, Body, Button, Icon , Container, Content} from "native-base";
-import { Link , Redirect, withRouter} from "react-router-native";
+import { Text, View } from "react-native";
+import {
+  Card,
+  CardItem,
+  Body,
+  Button,
+  Icon,
+  Container,
+  Content
+} from "native-base";
+import { Link, Redirect, withRouter } from "react-router-native";
 
-
-
-import { SideMenu, List, ListItem } from 'react-native-elements';
-import {observer} from "mobx-react";
+import { SideMenu, List, ListItem } from "react-native-elements";
+import { observer } from "mobx-react";
 import dataStore from "../stores/dataStore";
 import authStore from "../stores/authStore";
 
 class SlackBoxPlans extends Component {
-
-  renderItem(data){
+  renderItem(data) {
     return (
-
-          <Button
-            onPress = {() => {
-              dataStore.setIndex(data.id);
-              if (!authStore.isAuthenticated){
-              this.props.history.push('/Login')
-              }
-
-              else {
-                this.props.history.push('/plansDetail')
-              }
-            }}
-
-            icon block success>
-            <Icon name='person' />
-            <Text>{data.name}</Text>
-          </Button>
-
-
+      <Button
+        onPress={() => {
+          dataStore.setIndex(data.id);
+          if (!authStore.isAuthenticated) {
+            this.props.history.push("/Login");
+          } else {
+            this.props.history.push("/plansDetail");
+          }
+        }}
+        icon
+        block
+        success
+      >
+        <Icon name="person" />
+        <Text>{data.name}</Text>
+      </Button>
     );
-
   }
-
 
   render() {
-    const plans = dataStore.plans.map (data => this.renderItem(data));
+    const plans = dataStore.plans.map(data => this.renderItem(data));
 
-    return (
-
-      <List>{plans}</List>
-
-    );
+    return <List>{plans}</List>;
   }
 }
-export default  withRouter(observer(SlackBoxPlans));
+export default withRouter(observer(SlackBoxPlans));
